4fa90164be07b4eb9dd2e9dd020cdffb117e2803
Arroz-dev <arrozpapu202123@gmail.com>
2024-09-29 20:55:01 -0600

    subiendo la pagina

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('learn-more').addEventListener('click', function() {
+    alert('Mi visión para la escuela es hacer de este un espacio inclusivo, donde todos los estudiantes tengan oportunidades de crecer académicamente, socialmente y personalmente.');
+});
